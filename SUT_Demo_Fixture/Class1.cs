6106fe2138dece95e2d9258a909c296a9aed1717
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-03-22 15:56:22 +0000

    Fixture Added.

---
+++ SUT_Demo_Fixture/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SUT_Demo;
+using fit;
+using fit.Fixtures;
+
+namespace SUT_Demo_Fixture
+{
+    public class Class1 : ColumnFixture
+    {
+        public int x;
+        public int y;
+        public SUT_Demo.Program sut = new SUT_Demo.Program();
+
+        public int Add()
+        {
+            return sut.Add(x, y);
+        }
+    }
+}
